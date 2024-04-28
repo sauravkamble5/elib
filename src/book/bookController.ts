7df36cc4874e8385a5e5d13762243d0f5d5a5f00
Saurav Kamble <sauravkamble.dev@gmail.com>
2024-04-28 12:31:22 +0530

    uploading files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,55 @@
 import { NextFunction, Request, Response } from "express";
+import path from "node:path";
+import cloudinary from "../config/cloudinary";
+import createHttpError from "http-errors";
 
-const createBook = (req: Request, res: Response, next: NextFunction) => {
-  const {} = req.body;
-  
-  res.json({});
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  // const {} = req.body;
+  console.log("files", req.files);
+
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+  const coverImageType = files.coverImage[0].mimetype.split("/").at(-1);
+
+  const fileName = files.coverImage[0].filename;
+
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
+  const uploadResult = await cloudinary.uploader.upload(filePath, {
+    filename_override: fileName,
+    folder: "book-covers",
+    format: coverImageType,
+  });
+
+  const bookFileName = files.file[0].filename;
+  const bookFilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    bookFileName
+  );
+
+  try {
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: "pdf",
+      }
+    );
+
+    console.log("bookFileUploadResult", bookFileUploadResult);
+    console.log("uploadResult", uploadResult);
+    res.json({});
+  } catch (error) {
+    console.error(error);
+    return next(createHttpError(500, "Error while uploading the files"));
+  }
 };
 
 export { createBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -7,12 +7,16 @@ const bookRouter = express.Router();
 
 const upload = multer({
   dest: path.resolve(__dirname, "../../public/data/uploads"),
-  limits: { fields: 3e7 },
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
 });
 
-bookRouter.post("/", upload.fields([
-    {name: "coverImage", maxCount:1},
-    {name:"file", maxCount:1},
-]), createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default bookRouter;
